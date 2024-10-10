7401e0f5285cda37ea0e055f03aaa958c6443b84
JIEUNI <jieundaeun@naver.com>
2024-10-11 00:47:58 +0900

    [add] #1 color 추가

--- app/src/main/java/org/sopt/and/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/and/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package org.sopt.and.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Wavve
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// GrayScale
+val Gray100 = Color(0xFF929292)
+val Gray200 = Color(0xFF595959)
+val Gray300 = Color(0xFF393939)
+
+// Button
+val BlueButton = Color(0xFF2E56FF)
