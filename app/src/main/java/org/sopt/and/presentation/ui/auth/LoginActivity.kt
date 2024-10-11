e0c5c8ca0ae871196d0710a3237d46a756a20f21
JIEUNI <jieundaeun@naver.com>
2024-10-11 14:36:58 +0900

    [mod] #1 LoginActivity 함수화

--- app/src/main/java/org/sopt/and/presentation/ui/auth/LoginActivity.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/auth/LoginActivity.kt
@@ -8,14 +8,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,11 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import org.sopt.and.R
 import org.sopt.and.presentation.ui.component.TextWithHorizontalDivider
 import org.sopt.and.presentation.ui.component.ThreeTextWithVerticalDivider
@@ -36,7 +30,6 @@ import org.sopt.and.presentation.ui.component.WaveAllTopBar
 import org.sopt.and.presentation.ui.component.WaveTextField
 import org.sopt.and.presentation.ui.component.WaveTextFieldWithShowAndHide
 import org.sopt.and.ui.theme.ANDANDROIDTheme
-import org.sopt.and.ui.theme.Gray100
 import org.sopt.and.ui.theme.GrayBlack
 
 class LoginActivity : ComponentActivity() {
@@ -98,50 +91,7 @@ fun LoginScreen() {
             modifier = Modifier.height(50.dp),
             dividerText = stringResource(R.string.login_social_account)
         )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            // 소셜로그인 이미지 대신 해당 icon으로 대체
-            Icon(
-                painter = painterResource(R.drawable.ic_support_24),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 10.dp),
-                tint = Gray100
-            )
-            Icon(
-                painter = painterResource(R.drawable.ic_support_24),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 10.dp),
-                tint = Gray100
-            )
-            Icon(
-                painter = painterResource(R.drawable.ic_support_24),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 10.dp),
-                tint = Gray100
-            )
-            Icon(
-                painter = painterResource(R.drawable.ic_support_24),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 10.dp),
-                tint = Gray100
-            )
-            Icon(
-                painter = painterResource(R.drawable.ic_support_24),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 10.dp),
-                tint = Gray100
-            )
-        }
-        Text(
-            text = stringResource(R.string.social_account_information),
-            modifier = Modifier.padding(vertical = 10.dp),
-            color = Gray100,
-            fontSize = 12.sp
-        )
+        SocialLoginRow()
     }
 
 }
