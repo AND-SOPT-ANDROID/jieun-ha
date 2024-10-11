0e71ee0af7efede29eab2d408b651972f8d7804e
JIEUNI <jieundaeun@naver.com>
2024-10-12 03:51:30 +0900

    [del] #1 사용하지 않는 파일 삭제

--- app/src/main/java/org/sopt/and/presentation/ui/auth/RegisterEvent.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.and.presentation.ui.auth
-
-sealed class RegisterEvent {
-    data class EmailChanged(val email: String): RegisterEvent()
-    data class PasswordChanged(val password: String): RegisterEvent()
-    data object PasswordVisibilityChanged: RegisterEvent()
-}
--- app/src/main/java/org/sopt/and/presentation/ui/auth/RegisterState.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.and.presentation.ui.auth
-
-data class RegisterState(
-    val email: String = "",
-    val password: String = "",
-    val showPassword: Boolean = false
-)
