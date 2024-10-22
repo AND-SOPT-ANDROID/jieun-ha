6ef516c98d7e989bc4fb4b1404bf3256f5a7afa7
JIEUNI <jieundaeun@naver.com>
2024-10-22 22:49:17 +0900

    [feat] #1 로그인 뷰 - LoginContract 정의 및 LoginViewModel 수정

--- app/src/main/java/org/sopt/and/presentation/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/auth/login/LoginViewModel.kt
@@ -1,16 +1,51 @@
 package org.sopt.and.presentation.ui.auth.login
 
-import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.sopt.and.data.datasource.local.WaveLocalDataSource
+import org.sopt.and.presentation.util.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val localDataSource: WaveLocalDataSource
-) : ViewModel() {
+) : BaseViewModel<LoginContract.LoginEvent, LoginContract.LoginState, LoginContract.LoginEffect>() {
 
-    fun checkIsUserEmail(email: String): Boolean = (email == localDataSource.userEmail)
+    override fun createInitialState(): LoginContract.LoginState {
+        return LoginContract.LoginState()
+    }
 
-    fun checkIsUserPassword(password: String): Boolean = (password == localDataSource.userPassword)
+    override fun handleEffect(effect: LoginContract.LoginEffect) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun handleEvent(event: LoginContract.LoginEvent) {
+        when (event) {
+            is LoginContract.LoginEvent.EmailChanged -> {
+                setState(currentUiState.copy(email = event.email))
+            }
+
+            is LoginContract.LoginEvent.PasswordChanged -> {
+                setState(currentUiState.copy(password = event.password))
+            }
+
+            is LoginContract.LoginEvent.PasswordVisibilityChanged -> {
+                setState(currentUiState.copy(showPassword = !currentUiState.showPassword))
+            }
+
+            is LoginContract.LoginEvent.OnLoginBtnClicked -> {
+                if (checkIsUserEmail() && checkIsUserPassword()) {
+                    setEffect(LoginContract.LoginEffect.ShowSuccessSnackBar(successMessage = event.successMessage))
+                    setState(currentUiState.copy(loginStatus = LoginContract.LoginStatus.Success))
+                } else {
+                    setEffect(LoginContract.LoginEffect.ShowFailSnackBar(failMessage = event.failMessage))
+                    setState(currentUiState.copy(loginStatus = LoginContract.LoginStatus.Fail))
+                }
+            }
+        }
+    }
+
+    private fun checkIsUserEmail(): Boolean = (currentUiState.email == localDataSource.userEmail)
+
+    private fun checkIsUserPassword(): Boolean =
+        (currentUiState.password == localDataSource.userPassword)
 }
