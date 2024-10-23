28c9293a7d8939455eb7e90a820198a85ae98c4f
JIEUNI <jieundaeun@naver.com>
2024-10-23 23:52:24 +0900

    [feat] #4 HomeNavigator 구현

--- app/src/main/java/org/sopt/and/presentation/ui/home/HomeRoute.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/home/HomeRoute.kt
@@ -1,8 +1,13 @@
 package org.sopt.and.presentation.ui.home
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun HomeRoute() {
-   HomeScreen()
+fun HomeRoute(
+    paddingValues: PaddingValues
+) {
+    HomeScreen(modifier = Modifier.padding(paddingValues))
 }
--- app/src/main/java/org/sopt/and/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/home/HomeScreen.kt
@@ -1,13 +1,29 @@
 package org.sopt.and.presentation.ui.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
 @Composable
-fun HomeScreen() {
-    Text("Home Screen")
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 15.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text("Home Screen")
+    }
 }
 
 @Preview(showBackground = true)
