2d184a0d02a452a13d763d8c5b75aaeb709f47e3
JIEUNI <jieundaeun@naver.com>
2024-11-04 22:57:03 +0900

    [feat] #8 LocalDataStorage에 accessToken 필드 추가

--- app/src/main/java/org/sopt/and/data/datasource/local/WaveLocalDataSource.kt
+++ app/src/main/java/org/sopt/and/data/datasource/local/WaveLocalDataSource.kt
@@ -4,4 +4,5 @@ interface WaveLocalDataSource {
     var isLogin: Boolean
     var userEmail: String
     var userPassword: String
+    var accessToken: String
 }
--- app/src/main/java/org/sopt/and/data/datasourceImpl/local/WaveLocalDataSourceImpl.kt
+++ app/src/main/java/org/sopt/and/data/datasourceImpl/local/WaveLocalDataSourceImpl.kt
@@ -26,10 +26,15 @@ class WaveLocalDataSourceImpl @Inject constructor(
         get() = pref.getString(USER_PASSWORD, "") ?: ""
         set(value) = pref.edit { putString(USER_PASSWORD, value) }
 
+    override var accessToken: String
+        get() = pref.getString(ACCESS_TOKEN, "") ?: ""
+        set(value) = pref.edit { putString(ACCESS_TOKEN, value) }
+
     companion object {
         const val FILE_NAME = "AuthSharedPreferences"
         const val AUTO_LOGIN = "AutoLogin"
         const val USER_EMAIL = "UserEmail"
         const val USER_PASSWORD = "UserPassword"
+        const val ACCESS_TOKEN = "AccessToken"
     }
 }
